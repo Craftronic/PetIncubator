a0445ad52ea107b4c5909bb549e5d373ac15197b
username <username@users.noreply.github.com>
2024-01-15 02:00:42 +0100

    build js with fastclick

--- web-gui/src/main.tsx
+++ web-gui/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import initReactFastclick from 'react-fastclick'
+initReactFastclick();
 
 ReactDOM.createRoot(document.querySelector('esp-app')!).render(
   <React.StrictMode>
